0aafae2d109afc5fbfe23ba50589713d3642ba49
Praesidi <edik.halilov1999@gmail.com>
2022-07-28 11:31:19 +0300

    Create basic page with simple styling

---
+++ script.js
@@ -0,0 +1,52 @@
+use strict;
+
+// 0 = Rock, 1 = Paper, 2 - Scissors
+let computerPoints = 0;
+let playerPoints = 0;
+let playerChoice = "";
+
+
+function playerSelection () {
+  document.getElementById("")
+}
+
+function computerSelection () {
+  let randomNum = Math.floor(Math.random()*3);
+
+  if (randomNum = 0) {
+    let compChoice = "rock";
+    return compChoice;
+  }
+  else if (randomNum = 1) {
+    let compChoice = "paper";
+    return compChoice;
+  }
+  else {
+    let compChoice = "scissors";
+    return compChoice;
+  }
+}
+
+function gameResult (computerSelection (), playerSelection ()) {
+
+}
+
+function playerScore (playerPoints) {
+  playerPoints += 1;
+  return playerPoints;
+}
+
+function computerScore (computerPoints) {
+  let compScore += 1;
+  return computerPoints;
+}
+
+function newGame () {
+
+}
+
+function game (){
+
+}
+
+game ();
