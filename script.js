af440747dfd2be02abc59d3d293739262e3363b2
Praesidi <edik.halilov1999@gmail.com>
2022-07-31 13:12:06 +0300

    Finish the logic and fix bugs

--- script.js
+++ script.js
@@ -1,83 +1,104 @@
-use strict;
-
 let computerPoints = 0;
 let playerPoints = 0;
-let roundScore = 0;
-let playerSelection = "";
-let computerSelection = "";
-
-function getPlayerChoice () {
-  document.getElementById("")
-}
+let playerSelection = " ";
+//let computerSelection = "";
+const buttons = document.querySelectorAll("button");
+//const buttons = document.querySelector('.button');
 
 function getComputerChoice () {
   let randomNum = Math.floor(Math.random()*3);
+  //console.log(randomNum);
 
-  if (randomNum == 0) {
-    computerSelection = "rock";
-    return computerSelection;
-  }
-  else if (randomNum == 1) {
-    computerSelection = "paper";
-    return computerSelection;
-  }
-  else {
-    computerSelection = "scissors";
-    return computerSelection;
+  switch (randomNum) {
+    case (0):
+      computerSelection = "rock";
+      return computerSelection;
+      break;
+
+    case(1):
+      computerSelection = "paper";
+      return computerSelection;
+      break;
+
+    case(2):
+      computerSelection = "scissors";
+      return computerSelection;
+      break;
+
+    default:
+      console.log("Oops! Something went wrong D;");
   }
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection) {
+  const computerSelection = getComputerChoice();
 
-  getComputerChoice();
-
-  if ((playerSelection == "rock" && computerSelection == "scissors") ||    (playerSelection == "paper" && computerSelection == "rock") ||
+  if ((playerSelection == "rock" && computerSelection == "scissors") ||
+  (playerSelection == "paper" && computerSelection == "rock") ||
   (playerSelection == "scissors" && computerSelection == "paper")){
-    playerWinsRound();
-    roundScore++;
-  }
 
+    playerPoints++;
+    document.querySelector("#playerScore").innerHTML = "Your score: " + playerPoints;
+    document.querySelector("#computerScore").innerHTML = "Computer's score: " + computerPoints;
+    document.querySelector("#message").innerHTML = "Wow! Nice one. This round is yours";
+
+    if (playerPoints == 5) {
+      document.querySelector("#message").innerHTML = "You are the winner!";
+      setTimeout(newGame, 2000);
+      //newGame();
+    }
+  }
   else if ((playerSelection == "rock" && computerSelection == "paper") ||
   (playerSelection == "paper" && computerSelection == "scissors") ||
   (playerSelection == "scissors" && computerSelection == "rock")){
-    computerWinsRound();
-    roundScore++;
-  }
 
-  else {
-    document.querySelector("#message").innerHTML = "It's a tie!";
-  }
+    computerPoints++;
+    document.querySelector("#playerScore").innerHTML = "Your score: " + playerPoints;
+    document.querySelector("#computerScore").innerHTML = "Computer's score: " + computerPoints;
+    document.querySelector("#message").innerHTML = "This time I am better than you. Ha-ha!";
+
+    if (computerPoints == 5) {
+      document.querySelector("#message").innerHTML = "Not this time, buddy...";
+      setTimeout(newGame, 2000);
+      //newGame();
+    }
+  }else{
+      document.querySelector("#message").innerHTML = "It's a tie!";
+    }
 }
 
-function playerWinsRound (playerPoints) {
-  playerPoints += 1;
+/*function playerWinsRound (playerPoints) {
+  playerPoints++;
   document.querySelector("#playerScore").innerHTML = "Your score: " + playerPoints;
   document.querySelector("#message").innerHTML = "Wow! Nice one. This round is yours";
 }
 
 function computerWinsRound (computerPoints) {
-  computerPoints += 1;
+  computerPoints++;
   document.querySelector("#computerScore").innerHTML = "Computer's score: " + computerPoints;
   document.querySelector("#message").innerHTML = "This time I am better than you. Ha-ha!";
-}
+}*/
 
 function newGame () {
-  computerPoints = 0;
-  playerPoints = 0;
-  roundScore = 0;
-  playerSelection = "";
-  computerSelection = "";
-  document.querySelector("#message").innerHTML = "";
+  if (window.confirm ("Wanna play another round?")){
+    computerPoints = 0;
+    playerPoints = 0;
+    playerSelection = "";
+    computerSelection = "";
+    document.querySelector("#message").innerHTML = "";
+    document.querySelector("#playerScore").innerHTML = "Your Score: " + 0;
+    document.querySelector("#computerScore").innerHTML = "Computer's Score: " + 0;
+  }else{
+    alert ("Feel free to reload page if you wanna play again :)");
+    buttons.forEach(button =>{
+      button.disabled = true;
+    });
+  }
 }
 
-function game (roundScore){
-  if (roundScore =< 5){
-    playRound();
-  }
-  else {
-    newGame();
-  }
-
-}
-
-game();
+buttons.forEach(button =>{
+    button.addEventListener("click", function(){
+        playRound(button.value)
+        //console.log(button.value);
+    });
+})
